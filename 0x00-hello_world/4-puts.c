0339c36a5340fda4e1a339e5d73dab02c939a42f
amh21-Ha <amha.ha07@gmail.com>
2022-09-08 12:02:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main function
- * return - success(0)
+ * main - Entry point
+ * return - Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
