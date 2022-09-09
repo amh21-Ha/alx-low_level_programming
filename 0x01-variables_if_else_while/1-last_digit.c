e24edbc31cca6290e9888dede5af05ece85bc086
amh21-Ha <amha.ha07@gmail.com>
2022-09-09 11:38:25 -0700

    if -else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* My code */
-	printf("%s %d is %d and is", last, n, n % 10);
+	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
