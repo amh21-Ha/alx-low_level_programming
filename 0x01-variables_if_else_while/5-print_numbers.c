8b86080e8f21e02cb0e7571f1ff06d32c6fbab4c
amh21-Ha <amha.ha07@gmail.com>
2022-09-09 12:47:57 -0700

    sindle digits from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digites starting from 0 base 10.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	in n;
+
+	for (n = 0; n  < 10; n++)
+	{
+		printf("%d", n)
+	}
+	printf('\n');
+	return (0);
+}
