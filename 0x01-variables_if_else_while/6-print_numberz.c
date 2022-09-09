803daeb0fc7c3a455685e9f1e75e08230b9192b9
amh21-Ha <amha.ha07@gmail.com>
2022-09-09 13:03:02 -0700

    print single digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digites starting from 0 base 10.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n  < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
