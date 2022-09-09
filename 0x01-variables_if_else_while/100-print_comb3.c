bdcb477e597c77803013ea1e70d8e3d8a25985c4
amh21-Ha <amha.ha07@gmail.com>
2022-09-09 14:30:32 -0700

    combination of differnce two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = '0';
+	int b;
+
+	while (a <= '9') /* 57 is corrrsponds with ASCII character 9 */
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			putchar (a);
+			putchar (b);
+			if(a == '8' && b == '9')
+			{
+				putchar ('\n');
+			}
+			else
+			{
+				putchar (',');
+				putchar (' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
