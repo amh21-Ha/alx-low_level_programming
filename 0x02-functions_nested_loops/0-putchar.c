afa7ae358d9a451c28210252ec0ae20a40a58038
amh21-Ha <amha.ha07@gmail.com>
2022-09-14 11:26:23 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
 #include <main.h>"
 
 /**
- *
- *  * main - Print "_putchar" followed by a new line.
- *
- *   * Description: You are not allowed to include standard libraries.
- *
- *    * Return: 0
- *
- *     */
+ *   main - Print "_putchar" followed by a new line.
+ *   Description: You are not allowed to include standard libraries.
+ *    Return: 0
+ */
 
 int main(void)
-
 {
 	char holberton[] = "_putchar";
-
 	int i = 0;
+
 	while (holberton[i] != '\0')
 	{
 		_putchar(holberton[i]);
