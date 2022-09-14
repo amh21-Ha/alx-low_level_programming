2c7f05363e470dd514ff60efb3c652bf30ad0f7d
amh21-Ha <amha.ha07@gmail.com>
2022-09-14 13:23:16 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char c;
-	
 	while (i < 10)
 	{
 		c = 'a';
 		while (c <= 'z')
 		{
-			_putchar(c);																c++;
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
