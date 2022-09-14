0962bffc08bb334ba0c3662e22114280737f7fd8
amh21-Ha <amha.ha07@gmail.com>
2022-09-14 12:26:27 -0700

    convert to lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,10 @@
  *  print_alphabet - Print alphabet in lowercase using only `_putchar`
  *  Return: 0
  */
-
 void print_alphabet(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		_putchar(c);
