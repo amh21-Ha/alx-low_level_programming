c87b365334d29e56e77aceefe26756fe02135f41
amh21-Ha <amha.ha07@gmail.com>
2022-09-14 21:40:18 -0700

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 {
 	int x = 0;
 	int a = 0, b = 0, c = 0, d = 0;
-	
+
 	while (x < 1440)
 	{
 		_putchar(a + '0');
@@ -17,7 +17,8 @@ void jack_bauer(void)
 		_putchar(':');
 		_putchar(c + '0');
 		_putchar(d + '0');
-		_putchar('\n');d++;
+		_putchar('\n');
+		d++;
 	        if ( d > 9)
 		{
 			d = 0;
