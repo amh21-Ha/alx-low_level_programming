57d5e434e77a5ea238efb7b229cdaeffd49024fa
amh21-Ha <amha.ha07@gmail.com>
2022-09-21 09:58:37 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 	int l;
 	int i;
-	
+
 	for (l = 0; dest[l] != '\0'; l++)
 	{
 	}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,50 +4,48 @@
 
 int _putchar(char c);
 
-void reset_to_98(int *n);
+void print_alphabet(void);
 
-void swap_int(int *a, int *b);
+void print_alphabet_x10(void);
 
-int _strlen(char *s);
+int _islower(int c);
 
-void _puts(char *str);
+int _isalpha(int c);
 
-void print_rev(char *s);
+int print_sign(int n);
 
-void rev_string(char *s);
+int _abs(int);
 
-void puts2(char *str);
+int print_last_digit(int);
 
-void puts_half(char *str);
+void jack_bauer(void);
 
-void print_array(int *a, int n);
+void times_table(void);
 
-char *_strcpy(char *dest, char *src);
+int add(int, int);
 
-int _atoi(char *s);
+void print_to_98(int n);
 
-char *_strcat(char *dest, char *src);
+void print_times_table(int n);
 
-char *_strncat(char *dest, char *src, int n);
+int _isupper(int c);
 
-char *_strncpy(char *dest, char *src, int n);
+int _isdigit(int c);
 
-int _strcmp(char *s1, char *s2);
+void print_numbers(void);
 
-void reverse_array(int *a, int n);
+void print_triangle(int size);
 
-char *string_toupper(char *);
+void print_square(int size);
 
-char *cap_string(char *);
+void print_diagonal(int n);
 
-char *leet(char *);
+void print_line(int n);
 
-char *rot13(char *);
+void more_numbers(void);
 
-void print_number(int n);
+void print_most_numbers(void);
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-void print_buffer(char *b, int size);
+int mul(int a, int b);
 
 #endif
