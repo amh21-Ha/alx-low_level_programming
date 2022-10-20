236b328d8190fe16a2f455837a8a4ed694f10ae1
amh21-Ha <amha.ha07@gmail.com>
2022-10-20 01:36:05 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * myCleanupFun - function that prints somthing before
+ * the main function is executed.
+ */
+
+void myCleanupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
